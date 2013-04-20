d46a5a9e4f0db45f15c81c30cd31592e69441ef7
tony-azevedo <tazevedo@gmail.com>
2013-04-19 22:35:37 -0400

    For Kathy??

--- sandbox/PhototransductionSandbox.m
+++ sandbox/PhototransductionSandbox.m
@@ -3,7 +3,7 @@
 
 vre =  307; %s-1
 krmax = 11.4; %s-1
-krmin = krmax*3.3;
+krmin = krmax/3.3;
 ke = 6.5; % s-1
 FB = 59.4;
 
@@ -11,7 +11,7 @@ Vcyto = 1.4e-14;
 kcar = 5e-7; % M
 ncar = 2.6;
 
-G_0 = 3e-6; % M
+G_0 = 2.9313e-06; % M
 amin = 34.8e-8; % M/s
 amax = 34.8e-6; % M/s
 Kcyc = 4.8e-7; %M
@@ -28,7 +28,7 @@ KcG = 2e-5; %M
 Nav = 6.022e23; %Avagadro
 Fara = 96485; %/cm
 
-Ca_0 = 5e-7; %M
+Ca_0 = 5.0805e-07; %M
 fca = 0.16;
 jexsat = 4.8e-12; %pA
 Kex = 1.5e-6; %M
@@ -36,66 +36,288 @@ Bmax = 0;
 kplu1 = 0;
 kmin1 = 0;
 
-tstep = .00001;
-t = -.1:tstep:3; 
-I = zeros(size(t)); 
+tstep = .0001;
+t = -.5:tstep:3;
+I = zeros(size(t));
 R = I;
 E = I;
 G = ones(size(t)); G = G*G_0;
 Ca = ones(size(t)); Ca = Ca*Ca_0;
-a = ones(size(t)); 
+a = ones(size(t));
 b = ones(size(t));
 jcg = ones(size(t));
 jex = ones(size(t));
 CaBslow = zeros(size(t));
 
-I(t>=0&t<=.001) = 10;
-%%
-for i = 1:length(t);
+I(t>=0&t<=.001) = 1/tstep/10 * 10000;
+
+Caclamp = 1;
+
+% %%
+for i = 1:length(t)-1;
     % set
+    
     a(i) = amin+(amax-amin)/(1+(Ca(i)/Kcyc)^ncyc);
-    b(i) = (bdark + kcat/Vcyto*Nav*E(i))*(G(i)/(G(i)+Km));
-    jcg(i) = jmax*(G(i)^ncg/(G(i)^ncg+KcG^ncg))+jex(i);
+    b(i) = (bdark + kcat/Vcyto/Nav*E(i))*(G(i)/(G(i)+Km));
     jex(i) = jexsat* (Ca(i)/(Ca(i)+Kex));
+    jcg(i) = jmax*(G(i)^ncg/(G(i)^ncg+KcG^ncg))+jex(i);
     
     % update
-    dRdt = I - (krmin + (krmax - krmin)/(1+(Ca(i)/kcar)^ncar)) * R(i);
+    dRdt = I(i) - (krmin + (krmax - krmin)/(1+(Ca(i)/kcar)^ncar)) * R(i);
     R(i+1) = R(i)+ dRdt*tstep;
     
     dEdt = vre*R(i)-ke*E(i);
     E(i+1) = E(i)+ dEdt*tstep;
-
+    
     dGdt = a(i)-b(i);
     G(i+1) = G(i)+ dGdt*tstep;
-
-    dCadt = 1/(FB+1) *((1/2*fca*jcg(i) - jex(i))/Fara/Vcyto - kplu1*Ca(i)*(Bmax - CaBslow(i)) + kmin1*CaBslow(i));
+    if G(i+1)<0 G(i+1) = 0; end
+    
+    dCadt = 1/(FB+1) *((1/2*fca*jcg(i) - jex(i))/Fara/Vcyto); %- kplu1*Ca(i)*(Bmax - CaBslow(i)) + kmin1*CaBslow(i));
     Ca(i+1) = Ca(i)+ dCadt*tstep;
-
+    if Ca(i+1)<0 Ca(i+1) = 0; end
+    if Caclamp; Ca(i+1) = Ca(i); end
+    
     dCaBslowdt = kplu1*Ca(i)*(Bmax - CaBslow(i)) - kmin1*CaBslow(i);
-    CaBslow(i+1) = CaBslow(i)+ dCaBslowdt*tstep;    
+    CaBslow(i+1) = CaBslow(i)+ dCaBslowdt*tstep;
     
 end
+i = i+1;
+a(i) = amin+(amax-amin)/(1+(Ca(i)/Kcyc)^ncyc);
+b(i) = (bdark + kcat/Vcyto/Nav*E(i))*(G(i)/(G(i)+Km));
+jex(i) = jexsat* (Ca(i)/(Ca(i)+Kex));
+jcg(i) = jmax*(G(i)^ncg/(G(i)^ncg+KcG^ncg))+jex(i);
 
-%%
-figure(1) 
-subplot(10,1,1);
-plot(t,I), axis tight;
-subplot(10,1,2);
+% %%
+figure(1)
+
+subplot(6,2,1);
 plot(t,R), axis tight;
-subplot(10,1,3);
-plot(t,E), axis tight;
-subplot(10,1,4);
-plot(t,G), axis tight;
-subplot(10,1,5);
-plot(t,Ca), axis tight;
-subplot(10,1,6);
-plot(t,a), axis tight;
-subplot(10,1,7);
-plot(t,b), axis tight;
-subplot(10,1,8);
-plot(t,jcg), axis tight;
-subplot(10,1,9);
-plot(t,jex), axis tight;
-subplot(10,1,10);
-plot(t,CaBslow), axis tight;
+ylabel('R');
 
+subplot(6,2,3);
+plot(t,E), axis tight;
+ylabel('E');
+
+subplot(6,2,5);
+plot(t,a), axis tight;
+ylabel('cyclase');
+
+subplot(6,2,7);
+plot(t,b), axis tight;
+ylabel('hydrolysis');
+
+subplot(6,2,9);
+plot(t,G), axis tight;
+hold on
+ylabel('G');
+
+subplot(6,2,11);
+plot(t,Ca), axis tight;
+ylabel('Ca');
+
+
+
+subplot(3,2,2);
+plot(t,cumsum(I)*tstep), axis tight;
+ylabel('I');
+
+subplot(3,2,4);
+plot(t,jcg), hold on, axis tight;
+ylabel('pA');
+
+subplot(3,2,6);
+plot(t,jex), axis tight;
+ylabel('exchange');
+
+
+%% Simplified model.
+% Synapse model? Simplified. for Kathy
+% Ach - Ach is like E, gets released, goes up (can also get used up p*n).
+% R is like G, start with some available, then decreasing. ChE is clearing
+% Ach.  In this way it's creating more free receptors... like cyclase?
+
+bdark = 7e-5;
+Km = 1e-5; % M
+
+% rhydr = kcat/Vcyto/Nav; % Nav = 6.022e23; %Avagadro % Vcyto = 1.4e-14;% kcat = 2200;
+rhydr = 2.6095e-07;
+
+% decay
+kr = 7.3448; % kr = (krmin + (krmax - krmin)/(1+(Ca_0/kcar)^ncar)); krmax = 11.4; %s-1 % krmin = krmax/3.3; kcar = 5e-7; % M% ncar = 2.6;
+
+% creation of secondary product
+vre =  307; %s-1
+ke = 6.5; % s-1
+
+%creation
+a_0 = 1.5868e-05; % amin+(amax-amin)/(1+(Ca_0/Kcyc)^ncyc); amin = 34.8e-8; % M/s% amax = 34.8e-6; % M/s
+G_0 = 2.9313e-06; % M
+Ca_0 = 5.0805e-07; %M
+
+tstep = .0001;
+t = -.5:tstep:3;
+I = zeros(size(t));
+R = I;
+E = I;
+G = ones(size(t)); G = G*G_0;
+a = ones(size(t)); a = a*a_0;
+
+b = ones(size(t));
+
+I(t>=0&t<=.001) = 1/tstep/10 * 1250;
+
+Caclamp = 1;
+
+% %%
+for i = 1:length(t)-1;
+    % set
+    
+    b(i) = (bdark + rhydr*E(i))*(G(i)/(G(i)+Km));
+    
+    % update
+    dRdt = I(i) - kr * R(i);
+    R(i+1) = R(i)+ dRdt*tstep;
+    
+    dEdt = vre*R(i)-ke*E(i);
+    E(i+1) = E(i)+ dEdt*tstep;
+    
+    dGdt = a(i)-b(i);
+    G(i+1) = G(i)+ dGdt*tstep;
+    if G(i+1)<0 G(i+1) = 0; end
+    
+    
+end
+i = i+1;
+b(i) = (bdark + rhydr*E(i))*(G(i)/(G(i)+Km));
+
+% %%
+figure(1)
+
+subplot(2,2,1);
+plot(t,R), axis tight;
+hold on
+ylabel('R');
+
+subplot(2,2,2);
+plot(t,E), axis tight;
+hold on
+ylabel('E');
+
+% subplot(6,2,5);
+% plot(t,a), axis tight;
+% ylabel('cyclase');
+
+subplot(2,2,3);
+plot(t,b), axis tight;
+hold on
+ylabel('hydrolysis');
+
+subplot(2,2,4);
+plot(t,G), axis tight;
+hold on
+ylabel('G');
+
+% subplot(3,2,2);
+% plot(t,cumsum(I)*tstep), axis tight;
+% ylabel('I');
+
+
+%% Rework in terms of Fre AchC
+% Synapse model? Simplified. for Kathy
+% Ach - Ach is like E, gets released, goes up (can also get used up p*n).
+% R is like G, start with some available, then decreasing. ChE is clearing
+% Ach.  In this way it's creating more free receptors... like cyclase?
+
+bdark = 7e-5;
+Km = 1e-5; % M
+
+rhydr = 2.6095e-07;
+
+% decay
+kr = 7.3448; 
+
+% creation of secondary product
+vre =  307; %s-1
+ke = 6.5; % s-1
+
+%creation
+a_0 = 1.5868e-05; 
+N_a = 2.9313e-06; % M
+
+tstep = .0001;
+t = -.5:tstep:3;
+I = zeros(size(t));
+R = I;
+E = I;
+N_o = ones(size(t)); N_o = N_o*N_a;
+% unbinding rate
+a = ones(size(t)); a = a*a_0;
+
+b = ones(size(t));
+
+start = 0;
+finit = 1;
+I(t>=start & t<=finit) = 1/tstep/10 * 1;
+
+Caclamp = 1;
+
+% %%
+for i = 1:length(t)-1;
+    % set
+    
+    b(i) = (bdark + rhydr*E(i))*(N_o(i)/(N_o(i)+Km));
+    
+    % update
+    dRdt = I(i) - kr * R(i);
+    R(i+1) = R(i)+ dRdt*tstep;
+    
+    dEdt = vre*R(i)-ke*E(i);
+    E(i+1) = E(i)+ dEdt*tstep;
+    
+    dGdt = a(i)-b(i);
+    N_o(i+1) = N_o(i)+ dGdt*tstep;
+    if N_o(i+1)<0 N_o(i+1) = 0; end
+    
+    
+end
+i = i+1;
+b(i) = (bdark + rhydr*E(i))*(N_o(i)/(N_o(i)+Km));
+
+j = max(N_o)-N_o;
+
+% %%
+figure(1)
+
+subplot(3,2,1);
+plot(t,R), axis tight;
+hold on
+ylabel('R');
+
+subplot(3,2,2);
+plot(t,E), axis tight;
+hold on
+ylabel('E');
+
+% subplot(6,2,5);
+% plot(t,a), axis tight;
+% ylabel('cyclase');
+
+subplot(3,2,3);
+plot(t,b), axis tight;
+hold on
+ylabel('hydrolysis');
+
+subplot(3,2,4);
+plot(t,N_o), axis tight;
+hold on
+% plot(t,j);
+ylabel('G');
+
+subplot(3,2,5);
+plot(t,I), axis tight;
+hold on
+
+% subplot(3,2,2);
+% plot(t,cumsum(I)*tstep), axis tight;
+% ylabel('I');
---
+++ sandbox/SynapseSandbox2.m
@@ -0,0 +1,126 @@
+%% Rework in terms of Fre AchC
+% Synapse model? Simplified. for Kathy
+
+ex = 0;
+
+% Ach concentration at receptors
+Ach_per_ves =  10000;
+
+% vesicle release dynamics
+kr = 7.5; % s^-1
+kr = 7.5;%e-3; % s^-1
+tau_kr = 1/kr
+
+% diffusion|clearance rate 
+ke = 6.5e0; % s^-1
+ke = 6.5;%e-3; % s^-1
+tau_ke = 1/ke
+
+% rate of binding
+r_on = 0.25; % s^-1
+r_on = 0.25;%e-3; % s^-1
+tau_r_on = 1/r_on
+
+% unbinding rate - channels become available
+r_off = 16; % s^-1
+r_off = 160;%e-3; % s^-1
+tau_r_off = 1/r_off
+
+% Available channels
+N_0 = 20;
+
+% binding Michaelis constant (binding depends on how many channels are
+% available
+Km = 10; % M
+
+% no Ach initially
+A_0 = 0; 
+
+% binding has a dark value (inherent Ach or channel opening) - set so that
+% dN_a dt = 0 initially
+bdark = r_off/(N_0/(N_0+Km))
+
+%
+tstep = .0001; %100us
+t = -2:tstep:4;
+
+firing_rate = zeros(size(t));
+R = zeros(size(t)); % release
+A = zeros(size(t)); % [Ach]
+N_a = ones(size(t)); N_a = N_a*N_0; % channels available
+
+% binding (Ach+N_o - )
+b = ones(size(t));
+
+bolus = 500;  % spikes
+start = 0;
+finit = .5; % duration
+amp = bolus/(finit-start); % spikes/s
+firing_rate(t>=start & t<=finit) = amp;
+
+totalspikes = trapz(t,firing_rate)
+
+% %%
+for i = 1:length(t)-1;
+    % set
+    
+    % binding has a dark value (inherent Ach or channel opening)
+    b(i) = (bdark + r_on*A(i))*(N_a(i)/(N_a(i)+Km));
+    
+    % update
+    dRdt = firing_rate(i) - kr * R(i);
+    R(i+1) = R(i)+ dRdt*tstep;
+    
+    dAdt = Ach_per_ves*R(i)-ke*A(i);
+    A(i+1) = A(i)+ dAdt*tstep;
+    
+    dN_adt = r_off-b(i); % available channels increases with unbinding, decreases with binding
+    N_a(i+1) = N_a(i)+ dN_adt*tstep;
+    if N_a(i+1)<0 N_a(i+1) = 0; end
+    
+    
+end
+i = i+1;
+b(i) = (bdark + r_on*A(i))*(N_a(i)/(N_a(i)+Km));
+
+j = max(N_a)-N_a;
+
+% %%
+figure(1)
+bluelines = findobj(1,'Color',[0, 0, 1]);
+set(bluelines,'color',[.8 .8 1]);
+
+subplot(3,2,1);
+plot(t,firing_rate), axis tight;
+ylabel('firing_rate');
+hold on
+
+subplot(3,2,3);
+plot(t,R), axis tight;
+hold on
+ylabel('R - Release');
+
+subplot(3,2,2);
+plot(t,A), axis tight;
+hold on
+ylabel('E - Ach at channel');
+
+% subplot(6,2,5);
+% plot(t,r_off), axis tight;
+% ylabel('cyclase');
+
+subplot(3,2,4);
+plot(t,b), axis tight;
+hold on
+ylabel('b - binding');
+
+subplot(3,2,6);
+plot(t,N_a), axis tight;
+hold on
+% plot(t,j);
+ylabel('N_a - Channels free');
+
+
+% subplot(3,2,2);
+% plot(t,cumsum(I)*tstep), axis tight;
+% ylabel('I');
